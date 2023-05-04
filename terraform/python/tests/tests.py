aca22ef0cfb1e108dbdc7bd29832784ebf120d21
Stephanie Chester <schester81@gmail.com>
2023-05-04 14:07:50 -0700

    updated unit test

--- terraform/python/tests/tests.py
+++ terraform/python/tests/tests.py
@@ -1,14 +1,14 @@
+import unittest
 import boto3
 from unittest.mock import MagicMock
-import LambdaFunction # import your lambda function code here
-
-def test_lambda_handler():
+import LambdaFunction
+def lambda_handler():
     # Set up a mock DynamoDB client
     dynamodb = boto3.client('dynamodb', region_name='us-east-1')
-    LambdaFunction.dynamodb = MagicMock(return_value=dynamodb)
+    lambda_handler.dynamodb = MagicMock(return_value=dynamodb)
 
     # Call the lambda function
-    response = LambdaFunction.lambda_handler(event=None, context=None)
+    response = lambda_handler.lambda_handler(event=None, context=None)
 
     # Check the response
     assert response == {"statusCode": 200, "body": "Success"}
